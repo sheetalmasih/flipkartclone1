8a1711fb24fcf9f900e7685714d06e34334671c4
sheetalmasih <sheetalmasih66@gmail.com>
2023-10-30 09:31:27 +0530

    completed flipkartclone

---
+++ Data/feature.js
@@ -0,0 +1,74 @@
+export const feature=[
+    {
+        id:"feature1",
+        img:"https://rukminim1.flixcart.com/flap/64/64/image/29327f40e9c4d26b.png?q=100",
+        name:"Grocery",
+        link:"https://www.flipkart.com/grocery-supermart-store?marketplace=GROCERY&fm=neo%2Fmerchandising&iid=M_7ff7dc9e-a6ff-4a4b-9cff-9da37d7a8b7b_1_372UD5BXDFYS_MC.CBUR1Q46W5F1&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Grocery_CBUR1Q46W5F1&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L0_view-all&cid=CBUR1Q46W5F1",
+        subnavigation:false,
+
+    },
+    {
+        id:"feature2",
+        img:"https://rukminim1.flixcart.com/flap/64/64/image/22fddf3c7da4c4f4.png?q=100",
+        name:"Mobiles",
+        link:"https://www.flipkart.com/mobile-phones-store?fm=neo%2Fmerchandising&iid=M_9198a567-d7db-46f3-b7a5-641911d649ad_1_372UD5BXDFYS_MC.ZRQ4DKH28K8J&otracker=hp_rich_navigation_2_1.navigationCard.RICH_NAVIGATION_Mobiles_ZRQ4DKH28K8J&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_2_L0_view-all&cid=ZRQ4DKH28K8J",
+        subnavigation:false,
+    
+    },
+    {
+        id:"feature3",
+        img:"https://rukminim1.flixcart.com/fk-p-flap/64/64/image/0d75b34f7d8fbcb3.png?q=100",
+        name:"Fashion",
+        link:"https://www.flipkart.com/clothing-and-accessories/topwear/pr?sid=clo,ash&p[]=facets.ideal_for%255B%255D%3DMen&p[]=facets.ideal_for%255B%255D%3Dmen&otracker=categorytree&fm=neo%2Fmerchandising&iid=M_3de2bb9c-918b-4354-ae7f-8dd4a186933d_1_372UD5BXDFYS_MC.6XNZG1FYFBZT&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Fashion~Men%2527s%2BTop%2BWear_6XNZG1FYFBZT&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L1_view-all&cid=6XNZG1FYFBZT",
+        subnavigation:true,
+    
+    },
+    {
+        id:"feature4",
+        img:"https://rukminim1.flixcart.com/flap/64/64/image/69c6589653afdb9a.png?q=100",
+        name:"Electronics",
+        link:"https://www.flipkart.com/electronics-best-offers-store?fm=neo%2Fmerchandising&iid=M_989094b6-b179-4196-811a-6d14f9d1c255_1_372UD5BXDFYS_MC.KV8NA7Z3WYHI&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Electronics~Audio_KV8NA7Z3WYHI&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L1_view-all&cid=KV8NA7Z3WYH",
+        subnavigation:true,
+    
+    },
+    {
+        id:"feature5",
+        img:"https://rukminim1.flixcart.com/flap/64/64/image/ab7e2b022a4587dd.jpg?q=100",
+        name:"Home & Furniture",
+        link:"https://www.flipkart.com/india-ka-furniture-studio-store?fm=neo%2Fmerchandising&iid=M_1d97f608-f141-4743-9034-852dfb9da49c_1_372UD5BXDFYS_MC.Q6I8NGK1VUG9&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Home%2B%2526%2BFurniture~Home%2BFurnishings_Q6I8NGK1VUG9&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L1_view-all&cid=Q6I8NGK1VUG9",
+        subnavigation:true,
+    
+    },
+    {
+        id:"feature6",
+        img:"https://rukminim1.flixcart.com/flap/64/64/image/0ff199d1bd27eb98.png?q=100",
+        name:"Appliences",
+        link:"https://www.flipkart.com/tvs-and-appliances-new-clp-store?fm=neo%2Fmerchandising&iid=M_2bb8c169-d939-4cff-92d0-1b95c6cec617_1_372UD5BXDFYS_MC.LO4IWVHA61BX&otracker=hp_rich_navigation_6_1.navigationCard.RICH_NAVIGATION_Appliances_LO4IWVHA61BX&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_6_L0_view-all&cid=LO4IWVHA61BX",
+        subnavigation:false,
+    
+    },
+    {
+        id:"feature7",
+        img:"https://rukminim1.flixcart.com/flap/64/64/image/71050627a56b4693.png?q=100",
+        name:"Travel",
+        link:"https://www.flipkart.com/travel/flights?param=DTNavIcon&fm=neo%2Fmerchandising&iid=M_d05e3a10-f094-48a4-9d52-98f2ff8f560c_1_372UD5BXDFYS_MC.V4ZPKTOAO321&otracker=hp_rich_navigation_7_1.navigationCard.RICH_NAVIGATION_Travel_V4ZPKTOAO321&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_7_L0_view-all&cid=V4ZPKTOAO321",
+        subnavigation:false,
+    
+    },
+    {
+        id:"feature8",
+        img:"https://rukminim1.flixcart.com/flap/64/64/image/dff3f7adcf3a90c6.png?q=100",
+        name:"Beauty, Toys & More",
+        link:"https://www.flipkart.com/beauty-and-grooming/pr?sid=g9b&otracker=categorytree&p%5B%5D=facets.fulfilled_by%255B%255D%3DFlipkart%2BAssured&fm=neo%2Fmerchandising&iid=M_0df98dcf-b30f-4467-b29d-85c4663f0446_1_372UD5BXDFYS_MC.RXB0XORLSZ1I&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Beauty%252C%2BToys%2B%2526%2BMore~Beauty%2B%2526%2BPersonal%2BCare_RXB0XORLSZ1I&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L1_view-all&cid=RXB0XORLSZ1I",
+        subnavigation:true,
+    
+    },
+    {
+        id:"feature9",
+        img:"https://rukminim1.flixcart.com/fk-p-flap/64/64/image/05d708653beff580.png?q=100",
+        name:"Two Wheelers",
+        link:"https://www.flipkart.com/two-wheelers-store?fm=neo%2Fmerchandising&iid=M_0f1a791b-2f0e-46a5-b6b4-48a8356a142e_1_372UD5BXDFYS_MC.SCJPZ04TFJJM&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Two%2BWheelers~Petrol%2BVehicles_SCJPZ04TFJJM&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L1_view-all&cid=SCJPZ04TFJJM",
+        subnavigation:true,
+    
+    }
+]
---
+++ Data/imageSlider.js
@@ -0,0 +1,34 @@
+export const imageSlider = [
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/ee78de50f9dbe993.jpg?q=20",
+        link : "https://www.flipkart.com/mobile-phones-big-billion-days23-sneak-peek-jg39eowe-store?param=27890"
+    },
+    {
+        img:"https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/2f1b3957c095a754.jpg?q=20",
+        link:"https://www.flipkart.com/big-billion-days-store"
+    },
+    {
+        img:"https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/4cd6690ef44564f3.jpg?q=20",
+        link:"https://www.flipkart.com/travel/flights?param=FKHPtrTravel_CLP_DT-HPW_ZEROFEE-OUT-CONV-C-OR"
+    },
+    {
+        img:"https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/352e6f0f8034fab5.jpg?q=20",
+        link:"https://www.flipkart.com/travel/flights?param=Travel_FKHP_DT-HPW_NeverBefore1499"
+    },
+    {
+        img:"https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/7fd0e4ab26429926.jpg?q=20",
+        link:"https://www.flipkart.com/travel/flights?param=Travel_FKHP_DT-HPW_NeverBefore1499"
+    },
+    {
+        img:"https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/5df65ad101e18dbf.jpg?q=20",
+        link:"https://www.flipkart.com/6bo/g0i/9no/~cs-4iyl78rvxx/pr?sid=6bo,g0i,9no&collection-tab-name=Monitors+SPL&param=1234"
+    },
+    {
+        img:"https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/ad13d197ab22003b.png?q=20",
+        link:"https://www.flipkart.com/travel/flights?param=Travel_FTravel_FKHP_DT-HPW-spl-18off-banks"
+    },
+    {
+        img:"https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/352e6f0f8034fab5.jpg?q=20",
+        link:"https://www.flipkart.com/travel/flights?param=Travel_FKHP_DT-HPW_Akasa1299"
+    }
+]
